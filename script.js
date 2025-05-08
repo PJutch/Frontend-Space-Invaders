bfae7bb165e45ee168b014c4e250b5923891814d
PJutch <103033969+PJutch@users.noreply.github.com>
2025-05-08 21:05:31 +0300

    Add shooting

--- script.js
+++ script.js
@@ -13,15 +13,20 @@ const ctx = canvas.getContext('2d');
 
 class Player {
     x = canvas.width / 2;
-    y = canvas.height - 150;
+    y = canvas.height - 100;
 
     speed = 2;
     xVelocity = 0;
 
+    shotCooldownRemaining = 0;
+    shotCooldown = 60;
+
     size = 50;
     fillStyle = 'rgb(255 255 255)';
 
-    update() {
+    alive = true;
+
+    update(entities) {
         if (isKeyDown['d'] && !isKeyDown['a']) {
             this.xVelocity = this.speed;       
         } else if (isKeyDown['a'] && !isKeyDown['d']) {
@@ -38,25 +43,56 @@ class Player {
             this.x = 0;
         }
     
-        if (isKeyDown['w']) {
-            console.log('Pew-pew');
+        if (isKeyDown['w'] && this.shotCooldownRemaining <= 0) {
+            entities.push(new PlayerBullet(this.x + this.size / 2, this.y + this.size / 2));
+            this.shotCooldownRemaining = this.shotCooldown;
+        }
+        
+        if (this.shotCooldownRemaining) {
+            --this.shotCooldownRemaining;
         }
     }
 };
 
+class PlayerBullet {
+    x;
+    y;
+
+    speed = 5;
+
+    size = 20;
+    fillStyle = 'rgb(255 255 255)';
+
+    alive = true;
+
+    constructor(x, y) {
+        this.x = x - this.size / 2;
+        this.y = y - this.size / 2;
+    }
+
+    update() {
+        this.y -= this.speed;
+        if (this.y < 0) {
+            this.alive = false;
+        }
+    }
+}
+
 const player = new Player();
-const entities = [player];
+let entities = [player];
 
 function drawFrame() {
     ctx.fillStyle = 'rgb(0 0 127)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     for (entity of entities) {
-        entity.update();
+        entity.update(entities);
 
         ctx.fillStyle = entity.fillStyle;
         ctx.fillRect(entity.x, entity.y, entity.size, entity.size);
     }
+
+    entities = entities.filter((entity) => entity.alive);
 
     requestAnimationFrame(drawFrame);
 }
