ba0d385ac79f9d0b6650ed50790ffa249057cbd4
PJutch <103033969+PJutch@users.noreply.github.com>
2025-05-08 19:44:04 +0300

    Refactor player movement

--- script.js
+++ script.js
@@ -8,56 +8,54 @@ onkeyup = (e) => {
     isKeyDown[e.key] = false;
 }
 
-const canvas = document.querySelector(".game");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('.game');
+const ctx = canvas.getContext('2d');
 
-class Entity {
-    x;
-    y;
-    size;
-    fillStyle;
+class Player {
+    x = canvas.width / 2;
+    y = canvas.height - 150;
 
-    constructor(x, y, size, fillStyle) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.fillStyle = fillStyle;
+    speed = 2;
+    xVelocity = 0;
+
+    size = 50;
+    fillStyle = 'rgb(255 255 255)';
+
+    update() {
+        if (isKeyDown['d'] && !isKeyDown['a']) {
+            this.xVelocity = this.speed;       
+        } else if (isKeyDown['a'] && !isKeyDown['d']) {
+            this.xVelocity = -this.speed;
+        } else if (!isKeyDown['d'] && !isKeyDown['a']) {
+            this.xVelocity = 0;
+        }
+    
+        this.x += this.xVelocity;
+        if (this.x > canvas.width - this.size) {
+            this.x = canvas.width - this.size;
+        }
+        if (this.x < 0) {
+            this.x = 0;
+        }
+    
+        if (isKeyDown['w']) {
+            console.log('Pew-pew');
+        }
     }
 };
 
-const entities = [new Entity(canvas.width / 2, canvas.height - 150, 50, "rgb(255 255 255)")];
-const player = entities[0];
-
-const playerSpeed = 2;
-let playerVelocity = 0;
+const player = new Player();
+const entities = [player];
 
 function drawFrame() {
-    ctx.fillStyle = "rgb(0 0 127)";
+    ctx.fillStyle = 'rgb(0 0 127)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     for (entity of entities) {
+        entity.update();
+
         ctx.fillStyle = entity.fillStyle;
         ctx.fillRect(entity.x, entity.y, entity.size, entity.size);
-    }
-
-    if (isKeyDown['d'] && !isKeyDown['a']) {
-        playerVelocity = playerSpeed;       
-    } else if (isKeyDown['a'] && !isKeyDown['d']) {
-        playerVelocity = -playerSpeed;
-    } else if (!isKeyDown['d'] && !isKeyDown['a']) {
-        playerVelocity = 0;
-    }
-
-    player.x += playerVelocity;
-    if (player.x > canvas.width - player.size) {
-        player.x = canvas.width - player.size;
-    }
-    if (player.x < 0) {
-        player.x = 0;
-    }
-
-    if (isKeyDown['w']) {
-        console.log('Pew-pew');
     }
 
     requestAnimationFrame(drawFrame);
