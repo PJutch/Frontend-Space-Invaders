fc41b5ca1547af1a8176ab71b212f967ef9b6f0b
PJutch <103033969+PJutch@users.noreply.github.com>
2025-05-09 20:45:31 +0300

    Make shooting enemies shoot only if it's empty under them

--- script.js
+++ script.js
@@ -11,11 +11,19 @@ onkeyup = (e) => {
 const canvas = document.querySelector('.game');
 const ctx = canvas.getContext('2d');
 
+function areCollidingX(firstEntity, secondEntity) {
+    return firstEntity.x <= secondEntity.x && secondEntity.x < firstEntity.x + firstEntity.size
+        || secondEntity.x <= firstEntity.x && firstEntity.x < secondEntity.x + firstEntity.size;
+}
+
+function areCollidingY(firstEntity, secondEntity) {
+   return firstEntity.y <= secondEntity.y && secondEntity.y < firstEntity.y + firstEntity.size
+       || secondEntity.y <= firstEntity.y && firstEntity.y < secondEntity.y + firstEntity.size;
+}
+
 function areColliding(firstEntity, secondEntity) {
-    return (firstEntity.x <= secondEntity.x && secondEntity.x <= firstEntity.x + firstEntity.size
-         || secondEntity.x <= firstEntity.x && firstEntity.x <= secondEntity.x + firstEntity.size)
-        && (firstEntity.y <= secondEntity.y && secondEntity.y <= firstEntity.y + firstEntity.size
-         || secondEntity.y <= firstEntity.y && firstEntity.y <= secondEntity.y + firstEntity.size);
+    return areCollidingX(firstEntity, secondEntity) 
+        && areCollidingY(firstEntity, secondEntity);
 }
 
 class Player {
@@ -34,6 +42,7 @@ class Player {
     dead = false;
     isPlayerSide = true;
     invulnerable = false;
+    isSolid = true;
 
     respawnCooldownRemaining = 0;
     respawnCooldown = 120;
@@ -105,6 +114,7 @@ class Bullet {
     dead = false;
     isPlayerSide;
     invulnerable = false;
+    isSolid = false;
 
     constructor(x, y, isPlayerSide) {
         this.x = x - this.size / 2;
@@ -139,6 +149,7 @@ class MovingDownEnemy {
     dead = false;
     isPlayerSide = false;
     invulnerable = false;
+    isSolid = true;
 
     constructor(x, y) {
         this.x = x - this.size / 2;
@@ -176,6 +187,7 @@ class ShootingDownEnemy {
     dead = false;
     isPlayerSide = false;
     invulnerable = false;
+    isSolid = true;
 
     shotCooldown = 180;
     shotCooldownRemaining = this.shotCooldown / 2;
@@ -191,18 +203,29 @@ class ShootingDownEnemy {
             this.dead = true;
         }
 
-        if (this.shotCooldownRemaining <= 0) {
-            this.shotCooldownRemaining = this.shotCooldown;
-            entities.push(new Bullet(this.x + this.size / 2, this.y + this.size / 2, false))
-        } else {
-            this.shotCooldownRemaining -= enemySpeed;
-        }
-
+        let canShoot = true;
         for (let entity of entities) {
             if (!entity.invulnerable
              && areColliding(this, entity) && entity.isPlayerSide) {
                 this.dead = true;
                 entity.dead = true;
+            }
+
+            if (entity != this && entity.y > this.y && areCollidingX(this, entity) 
+             && entity.isSolid && !entity.isPlayerSide) {
+                canShoot = false;
+            }
+        }
+
+        if (canShoot) {
+            if (this.shotCooldownRemaining <= 0) {
+                this.shotCooldownRemaining = this.shotCooldown;
+                entities.push(new Bullet(
+                    this.x + this.size / 2, 
+                    this.y + this.size / 2, 
+                    false))
+            } else {
+                this.shotCooldownRemaining -= enemySpeed;
             }
         }
 
@@ -216,7 +239,7 @@ class ShootingDownEnemy {
 let lives = 3;
 
 const player = new Player();
-let entities = [player, new ShootingDownEnemy(canvas.width / 2, 30)];
+let entities = [player, new ShootingDownEnemy(canvas.width / 2, 100), new MovingDownEnemy(canvas.width / 2, 30)];
 
 function drawFrame() {
     ctx.fillStyle = 'rgb(0 0 127)';
